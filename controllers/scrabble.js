ebfae94a1d28ebf91c0223f5386e8e2be464b30b
sophia <sophiabaldonado@gmail.com>
2016-06-13 17:07:46 -0700

    Create chart route and view to show score chart;

--- controllers/scrabble.js
+++ controllers/scrabble.js
@@ -5,11 +5,11 @@ var ScrabbleController = {
   scrabbleTitle: 'SuperScrabbleScorer',
 
   getIndex: function (req, res, next) {
-    res.render('index', { title: this.scrabbleTitle })
+    res.render('index', { title: ScrabbleController.scrabbleTitle })
   },
 
   getScoreForm: function (req, res) {
-    res.render('score', { title: this.scrabbleTitle })
+    res.render('score', { title: ScrabbleController.scrabbleTitle })
   },
 
   postScore: function (req, res) {
@@ -17,7 +17,22 @@ var ScrabbleController = {
     locals.word = req.body.word
     locals.score = scrabble.score(req.body.word)
 
-    res.render('score', { title: this.scrabbleTitle, locals: locals })
+    res.render('score', { title: ScrabbleController.scrabbleTitle, locals: locals })
+  },
+
+  scoreChart: function (req, res) {
+    var locals = {}
+    // var chart_keys = []
+    // var chart_values
+    locals.chart_keys = []
+    locals.chart_values = []
+
+    for (var key in scrabble.score_key) {
+      locals.chart_keys.push(parseInt(key, 10))
+      locals.chart_values.push(scrabble.score_key[key])
+    }
+
+    res.render('scorechart', { title: ScrabbleController.scrabbleTitle, locals: locals })
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,6 @@ router.get('/', Controller.getIndex)
 router.get('/score', Controller.getScoreForm)
 router.post('/score', Controller.postScore)
 
+router.get('/chart', Controller.scoreChart)
+
 module.exports = router
